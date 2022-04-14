fd2e27c8eeb526cda05d81c109e3556a494c4824
Carmen-Wu <56771256+Carmen-Wu@users.noreply.github.com>
2022-04-13 22:20:47 -0400

    Added navigation bar to home page and added Home.css file for styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,22 +4,22 @@
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
+import './Home.css'; 
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <nav className='navBar'> 
+          <ul>
+            <li><Link to="/userProfile">User Profile</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/debits">Debits</Link></li>
+            <li><Link to="">Credits</Link></li>
+          </ul>
+        </nav>
         <img src="https://picsum.photos/200/200" alt="bank"/>
         <h1>Bank of React</h1>
-
-        <Link to="/userProfile">User Profile</Link>
-        <br/>
-        <Link to="/login">Login</Link>
-        <br/>
-        <Link to="">Credits (to be implemented in the Assignment)</Link>
-        <br/>
-        <Link to="/debits">Debits (to be implemented in the Assignment)</Link>
-        
         <AccountBalance accountBalance={this.props.accountBalance}/>
       </div>
     );
